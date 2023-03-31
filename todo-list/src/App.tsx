4796512d7fdc46766d2b82f1bfac733b96681c6f
radixdt2412 <radixdt.2412@gmail.com>
2023-03-31 18:30:41 +0530

    created todo list

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todo from './todo/Todo';
+
+function App() {
+  return (
+   <>
+   <Todo />
+   </>
+  );
+}
+
+export default App;
